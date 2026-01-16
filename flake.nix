49d3006a5580bcbbbce9d5295128e3a7a2d49831
gapuchi <arjun.adhia@gmail.com>
2026-01-16 11:47:32 +0530

    Each Default System

--- flake.nix
+++ flake.nix
@@ -3,34 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = 
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        buildInputs = with pkgs; [ texliveFull ];
-      };
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ texliveFull ];
+        };
 
-    packages.x86_64-linux.default = 
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.stdenvNoCC.mkDerivation rec {
-        name = "generate pdf";
-        src = self;
-        buildInputs = with pkgs; [ coreutils texliveFull ];
-        phases = ["unpackPhase" "buildPhase" "installPhase"];
-        buildPhase = ''
-          export PATH="${pkgs.lib.makeBinPath buildInputs}";
-          mkdir -p .cache/texmf-var
-          env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-            latexmk -interaction=nonstopmode -pdf resume.tex
-        '';
-        installPhase = ''
-          mkdir -p $out
-          cp resume.pdf $out/
-        '';
-      };
-  };
+        packages.default = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "generate-pdf";
+          src = self;
+          buildInputs = with pkgs; [ coreutils texliveFull ];
+          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf resume.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp resume.pdf $out/
+          '';
+        };
+      }
+    );
 }
